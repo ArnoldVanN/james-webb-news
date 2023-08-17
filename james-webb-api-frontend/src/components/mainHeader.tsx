bef0b628ad7037c92ae3bfa9a04647ae3b949758
ArnoldVanN <arnovannieuwenhuyzen@outlook.com>
2023-08-17 22:09:29 +0200

    Added border and slanted panel to header

--- james-webb-api-frontend/src/components/mainHeader.tsx
+++ james-webb-api-frontend/src/components/mainHeader.tsx
@@ -5,14 +5,14 @@ import Navbar from "../components/navbar"
 
 import "../styles/components/header.scss"
 
-import headerImage from "../images/james-webb-stars-header-image.jpg"
+import headerImage from "../images/main_image_star-forming_region_carina_nircam_final-5mb.jpg"
 
 const MainHeader = () => {
   return (
     <header>
       <Navbar />
       <div id="header-image-wrapper">
-        <img id="header-image" src={headerImage} alt="" />
+        <img src={headerImage} alt="" />
         <div className="centered">
           <h1>James Webb News API</h1>
         </div>
