183f75ab188d1b7b125f04121bb370e340ff8947
ArnoldVanN <arnovannieuwenhuyzen@outlook.com>
2023-08-27 21:57:56 +0200

    Added pathPrefix to gatsby-config.ts

--- james-webb-api-frontend/gatsby-config.ts
+++ james-webb-api-frontend/gatsby-config.ts
@@ -15,6 +15,8 @@ const config: GatsbyConfig = {
       }
     ]
   },
+  // Set path prefix for GitHub Actions/Pages
+  pathPrefix: "/james-webb-news", 
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
